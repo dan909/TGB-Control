7971fb7b96fa3027290b282e5c184d9292392c0d
Danny Awty-Carroll <dga1@aber.ac.uk>
2019-10-01 12:43:46 +0100

    more testing to pass times

--- ComsTest/ComsTestA/ComsTestA.ino
+++ ComsTest/ComsTestA/ComsTestA.ino
@@ -1,27 +1,48 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(11, 12); // RX, TX
+char bitein;
+char readin;
 
-void setup()
-{
+
+String printTime(int no) {
+  int H = floor(no/4);
+  int M = no % 4;
+  M = M * 15;
+  char Time[17];
+  sprintf(Time,"%02i:%02i",H,M);
+  String Ttime = String(Time);
+  return Time;
+}
+
+
+void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Native USB only
   }
 
-
-  Serial.println("Goodnight moon!");
+  Serial.println("Testing Coms...");
 
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
-  //mySerial.println("Hello, world?");
 }
 
-void loop() // run over and over
-{
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-  if (Serial.available())
-    mySerial.write(Serial.read());
+void loop() { // run over and over
+  delay(50);
+  if (mySerial.available()) {
+    delay(50);
+    bitein = mySerial.read();
+    Serial.println(bitein);
+    if (bitein == '/') {
+      Serial.println(readin);
+      Serial.println(printTime(int(readin)));
+      readin = '/';
+    } else {
+      readin = readin + bitein;
+    }
+    delay(50);
+    mySerial.write("OK");
+  }
 }
